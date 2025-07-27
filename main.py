b211fdbf5d59e66c73c0538b9cbd6fe4559b9578
VV <vv@MacBook-Air-VV.local>
2025-07-27 13:20:02 +0300

    обновил main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 from aiohttp import web
 
 TOKEN = os.getenv("BOT_TOKEN")
+if not TOKEN:
+    raise ValueError("BOT_TOKEN не найден в переменных окружения!")
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -21,14 +23,19 @@ async def start(msg: types.Message):
     await msg.answer(f"👋 Привет!, {msg.from_user.first_name}!\n\nДобро пожаловать!\n\nВыбери интересующий тебя пункт:", reply_markup=kb)
 
     async def on_startup(_):
-    await bot.set_webhook(url=f"https://{os.getenv('https://Telegram-bot.onrender.com')}/webhook")
+        webhook_url = f"https://{os.getenv( ' RENDER_EXTERNAL_URL ')}/webhook"
+        if not webhook_url or 'None' in webhook_url:
+        raise ValueError("RENDER_EXTERNAL_URL не настроен корректно!")
+    await bot.set_webhook(url=webhook_url)
+    print(f"Webhook set to {webhook_url}")
+    
 
     async def handle_request(request):
-    return web.Response(text="Webhook is running")
+        return web.Response(text="Webhook is running")
 
 app.router.add_get('/webhook', handle_request)
 app.router.add_post('/webhook', dp.webhook())
 
 if __name__ == '__main__':
-PORT = int(os.getenv("PORT", 10000))
+    PORT = int(os.getenv("PORT", 10000))
     web.run_app(app, host='0.0.0.0', port=PORT)
